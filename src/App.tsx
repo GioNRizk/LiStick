52b3d9e6fb0b99d208b3da1a7ecb33b891c96900
Marc Salameh <salamehmarc2001@gmail.com>
2025-09-08 10:35:06 +0300

    Li-Stick Website release V1.2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Header from "./components/common/Header";
 
 // Pages
-import HomePage from "./pages/Home";
+import HomePage from "../src/pages/Home";
 import About from "./pages/About";
 import FeaturesPage from "./pages/features";
 import ImpactPage from "./pages/impact";
